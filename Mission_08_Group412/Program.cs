e326f4f79c45fe643d6dc656130fe3ae6e11e254
Luke Woods <woodsluke23@gmail.com>
2024-02-27 08:49:18 -0700

    Got the connection string added and finalized models and DB

--- Mission_08_Group412/Program.cs
+++ Mission_08_Group412/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Mission_08_Group412.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Mission08Context>(options =>
+{
+    options.UseSqlite(builder.Configuration["ConnectionStrings:Mission08Connection"]);
+});
 
 var app = builder.Build();
 
