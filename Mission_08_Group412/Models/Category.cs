8931da26ecc7b46a28f605649544ec1b96714652
Luke Woods <woodsluke23@gmail.com>
2024-02-27 08:54:42 -0700

    Fixed some errors in the models and cleaned up the context file

--- Mission_08_Group412/Models/Category.cs
+++ Mission_08_Group412/Models/Category.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
 
-namespace Mission_08_Group412.Models;
-
-public class Category
+namespace Mission_08_Group412.Models
 {
-    [Key]
-    public int CategoryId { get; set; }
-    public string CategoryName { get; set; }
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
 
-}
+    }
 }
--- Mission_08_Group412/Models/Mission08Context.cs
+++ Mission_08_Group412/Models/Mission08Context.cs
@@ -4,45 +4,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Mission_08_Group412.Models;
 
-public partial class Mission08Context : DbContext
+public class Mission08Context : DbContext
 {
-    public Mission08Context()
+    public Mission08Context(DbContextOptions<Mission08Context> options) : base(options)
     {
-    }
 
-    public Mission08Context(DbContextOptions<Mission08Context> options)
-        : base(options)
-    {
     }
 
     public virtual DbSet<Category> Categories { get; set; }
-
     public virtual DbSet<ToDoList> ToDoLists { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlite("Data Source=Mission08.sqlite");
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Category>(entity =>
-        {
-            entity.ToTable("Category");
-
-            entity.Property(e => e.CategoryId).ValueGeneratedNever();
-        });
-
-        modelBuilder.Entity<ToDoList>(entity =>
-        {
-            entity.HasKey(e => e.TaskId);
-
-            entity.ToTable("ToDoList");
-
-            entity.HasOne(d => d.Category).WithMany(p => p.ToDoLists).HasForeignKey(d => d.CategoryId);
-        });
-
-        OnModelCreatingPartial(modelBuilder);
-    }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
